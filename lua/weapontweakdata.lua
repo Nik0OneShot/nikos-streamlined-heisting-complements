458dc1714ee78ecf0a14fa776f02ba7b5dec2cfe
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-01-29 08:47:55 +1030

    give miniguns suppression, give lmgs suppression
    lmgs and miniguns in vanilla are boring. they don't do anything unique compared to assault rifles that assault rifles can't already do. and, a majority of the time, they outperform them in every possible way.
    
    my change doesn't fix this, but it does give it at the very least something interesting. lmgs and miniguns now have passive suppression whenever you fire them near enemies, causing them to go into a panic. this is identical to how muscle/sociopaths panic works.

--- lua/weapontweakdata.lua
+++ lua/weapontweakdata.lua
@@ -57,10 +57,14 @@ Hooks:PostHook(WeaponTweakData, "init", "shc_init", function (self)
 				-- Add armor piercing to crossbows
 				weap_data.armor_piercing_chance = 1
 			elseif cat_map.minigun then
+					weap_data.panic_suppression_chance = 0.1
 				-- Buff minigun damage in exchange for falloff/movement speed penalty
 				if not weap_data.damage_falloff then
 					weap_data.stats.damage = math.ceil(weap_data.stats.damage * 1.2)
 					weap_data.damage_falloff = FALLOFF_TEMPLATE.SPECIAL_LOW
+				end
+				elseif cat_map.lmg then
+                   weap_data.panic_suppression_chance = 0.05
 				end
 			elseif cat_map.shotgun then
 				-- Restore shotgun pellets
