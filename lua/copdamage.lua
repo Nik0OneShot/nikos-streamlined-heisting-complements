c08629ba81859970c95353a10f235043087775df
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-17 20:57:59 +0930

    ????

--- lua/copdamage.lua
+++ lua/copdamage.lua
@@ -1,3 +1,5 @@
+-- ????
+
 Hooks:PostHook(CopDamage, "convert_to_criminal", "shc_convert_to_criminal", function(self)
 	self._damage_reduction_multiplier = self._damage_reduction_multiplier * 0.25
 end)
