bf924d5af47a30ad9dce7d0270d4f2921646bd43
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-17 22:40:13 +0930

    change every vehicles health to 5000 (excluding some) and change fov to 90.

---
+++ lua/vehicletweakdata.lua
@@ -0,0 +1,41 @@
+Hooks:PostHook(VehicleTweakData, "init', "makecarsepic", function(self)
+  -- change every car to have 5000 health and 90 fov.
+	self.falcogini.damage = { max_health = 5000 }
+  self.falcogini.fov = 90
+
+	self.muscle.damage = { max_health = 5000 }
+  self.muscle.fov = 90
+
+	self.forklift.damage = { max_health = 5000 }
+  self.forklift.fov = 90
+
+	self.forklift_2.damage = { max_health = 5000 }
+  self.forklift_2.fov = 90
+
+	self.box_truck_1.damage = { max_health = 5000 }
+  self.box_truck_1.fov = 90
+
+	self.mower_1.damage = { max_health = 5000 }
+  self.mower_1.fov = 90
+
+	self.boat_rib_1.damage = { max_health = 5000 }
+  self.boat_rib_1.fov = 90
+
+	self.blackhawk_1.damage = { max_health = 99999999 }
+  self.blackhawk_1.fov = 90
+
+	self.bike_1.damage = { max_health = 5000 }
+  self.bike_1.fov = 90
+
+	self.bike_2.damage = { max_health = 5000 }
+  self.bike_2.fov = 90
+
+	self.wanker.damage = { max_health = 50009999999 }
+  self.wanker.fov = 90
+
+	self.golfcart.damage = { max_health = 5000 }
+  self.golfcart.fov = 90
+
+	self.kubelwagen.damage = { max_health = 5000 }
+  self.kubelwagen.fov = 90
+end)
