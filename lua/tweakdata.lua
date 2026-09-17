3d0f49aff42c3ddddec9204c78267cf42f4c1255
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 02:16:50 +0930

    black lives matter more restoration mod code, this time for tweakdata

--- lua/tweakdata.lua
+++ lua/tweakdata.lua
@@ -1,2 +1,71 @@
 tweak_data.projectiles.rocket_ray_frag.damage = 5000 -- commando 101
 tweak_data.projectiles.rocket_frag.damage = 15000 -- hrl-7
+
+-- black lives matter more code from rrestoration mod for tweakdata
+
+tweak_data.casino = {
+	unlock_level = 0,
+	entrance_level = {
+		14,
+		28,
+		40,
+		45,
+		55,
+		65,
+		75
+	},
+	entrance_fee = {
+		15000,
+		15000,
+		15000,
+		15000,
+		15000,
+		15000,
+		15000
+	},
+	prefer_cost = 500000,
+	prefer_chance = 0.2,
+	secure_card_cost = {
+		1000000,
+		3300000,
+		6500000
+	},
+	secure_card_level = {
+		0,
+		0,
+		0
+	},
+	infamous_cost = 3000000,
+	infamous_chance = 3
+}	
+
+tweak_data.experience_manager.stage_failed_multiplier = 0.5
+-- From update 34
+tweak_data.experience_manager.stage_completion = {
+	200,
+	250,
+	300,
+	350,
+	425,
+	475,
+	550
+}
+tweak_data.experience_manager.job_completion = {
+	2000,
+	4000,
+	10000,
+	16000,
+	20000,
+	28000,
+	32000
+}
+
+tweak_data.experience_manager.pro_day_multiplier = {
+	1,
+	1,
+	1,
+	1,
+	1,
+	1,
+	1
+}
