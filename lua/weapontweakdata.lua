ea774317c31912aad6ffd763434ecaafd0c18c60
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-17 22:21:05 +0930

    change thanatos damage, commando 101 damage, and hrl-7 damage + reload.

--- lua/weapontweakdata.lua
+++ lua/weapontweakdata.lua
@@ -51,6 +51,17 @@ Hooks:PostHook(WeaponTweakData, "init", "shc_init", function (self)
 	-- chimano 88
 	self.glock_17.stats.damage = 72
 
+	-- thanatos .50 cal
+	self.m95.stats.damage = 5000
+	self.m95.stats_modifiers = { damage = 1 }
+
+	-- commando 101
+	self.ray.stats.damage = 5000 -- have to edit in tweakdata.lua to actually change the damage
+
+	-- hrl 7
+	self.rpg7.stats.reload = 21
+	self.rpg7.stats.damage = 15000 -- have to go into tweakdata.lua to actually change the damage.
+
 	-- Generic weapon stat changes
 	for weap_id, weap_data in pairs(self) do
 		if type(weap_data) == "table" and weap_data.stats then
