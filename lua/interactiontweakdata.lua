6a252d1a657f29bc4edc969acdfd7e5dbe938a8b
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 04:05:22 +0930

    more resmod interactions

--- lua/interactiontweakdata.lua
+++ lua/interactiontweakdata.lua
@@ -10,5 +10,35 @@ Hooks:PostHook(InteractionTweakData, "init", "gage packages xd xd xd uwuwuwwuwuw
 		{ upgrade = "pick_lock_easy_speed_multiplier", category = "player" },
 		{ upgrade = "pick_lock_speed_multiplier", category = "player" }
 	}
-    self.hack_ipad.timer = 6
+    self.hack_ipad.timer = 3
+	self.c4.timer = 2
+	self.c4_special.timer = 2
+	self.c4_bag.timer = 4
+	self.c4_x1_bag.timer = 4
+	self.shape_charge_plantable.timer = 2
+	self.hack_ipad_jammed.timer = 10
+	self.hack_suburbia.timer = 5
+	self.hack_suburbia_outline.timer = 5
+	self.hack_suburbia_jammed.timer = 5
+	self.security_station.timer = 3
+	self.security_station_keyboard.timer = 6
+	self.big_computer_hackable.timer = 6
+	self.big_computer_not_hackable.timer = 6
+	self.big_computer_server.timer = 6
+	self.security_station_jammed.timer = 10
+	self.hack_numpad.timer = 15
+	self.votingmachine2.timer = 5
+	self.votingmachine2_jammed.timer = 5
+	self.sc_tape_loop.timer = 2
+	self.hack_electric_box.timer = 6
+	self.hack_ship_control.timer = 6
+	self.timelock_hack.timer = 6
+	self.mcm_laptop.timer = 3
+	self.hack_skylight_barrier.timer = 6
+	self.drk_hold_hack_computer.timer = 10
+	self.hold_start_scan.timer = 3
+	self.hold_new_hack.timer = 3
+	self.hold_type_in_password.timer = 5
+	self.ranc_hold_enter_turret.timer = 0.5
+	self.take_pardons.timer = 0
 end)
