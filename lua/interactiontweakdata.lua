797af33b20609af1bc7aac42d3f1dab2fea3117d
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 03:51:54 +0930

    derp

--- lua/interactiontweakdata.lua
+++ lua/interactiontweakdata.lua
@@ -1,14 +1,14 @@
-Hooks:PostHook( InteractionTweakData, "init", "gage packages xd xd xd uwuwuwwuwuwuwuwuwuwuwuwuwuwuwuwuw and other stuff too #goon ", function(self)
+Hooks:PostHook(InteractionTweakData, "init", "gage packages xd xd xd uwuwuwwuwuwuwuwuwuwuwuwuwuwuwuwuw and other stuff too #goon", function(self)
 	self.gage_assignment.timer = 0
 	self.take_pardons.timer = 0
-  self.ordnance_bag.timer = 1.5
-  self.requires_ecm_jammer_atm.timer = 8
-  self.requires_ecm_jammer_double.timer = 4
-  self.shaped_sharge.timer = 2
+    self.ordnance_bag.timer = 1.5
+    self.requires_ecm_jammer_atm.timer = 8
+    self.requires_ecm_jammer_double.timer = 4
+    self.shaped_sharge.timer = 2
 	self.pick_lock_hard.timer = 90
 	self.pick_lock_hard.upgrade_timer_multipliers = {
 		{ upgrade = "pick_lock_easy_speed_multiplier", category = "player" },
 		{ upgrade = "pick_lock_speed_multiplier", category = "player" }
 	}
-  self.hack_ipad.timer = 
+    self.hack_ipad.timer = 6
 end)
