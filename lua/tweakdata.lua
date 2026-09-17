b20df9738c6cf76588d3e6d57954b10afeb03410
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-17 22:25:54 +0930

    swapped the two around

--- lua/tweakdata.lua
+++ lua/tweakdata.lua
@@ -1,2 +1,2 @@
-tweak_data.projectiles.rocket_ray_frag.damage = 15000 -- hrl-7
-tweak_data.projectiles.rocket_frag.damage = 5000 -- commando 101
+tweak_data.projectiles.rocket_ray_frag.damage = 5000 -- commando 101
+tweak_data.projectiles.rocket_frag.damage = 15000 -- hrl-7
