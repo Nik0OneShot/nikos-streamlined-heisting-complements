ee9af5efd151e92f27c83b5de2fa36254a6dd3a0
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 06:44:41 +0930

    comma that doesn't need to be there

--- lua/tweakdata.lua
+++ lua/tweakdata.lua
@@ -130,4 +130,4 @@ tweak_data.experience_manager.limited_xmas_bonus_multiplier = 1.5
 tweak_data:digest_recursive(tweak_data.experience_manager)
 
 -- lets you pick death wish and death sentence off the bat
-tweak_data.difficulty_level_locks = { 0, 0, 0, 0, 0, 0, 0, 0, }
+tweak_data.difficulty_level_locks = { 0, 0, 0, 0, 0, 0, 0, 0 }
