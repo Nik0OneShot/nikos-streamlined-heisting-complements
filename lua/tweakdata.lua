828f56bf9de1aee54a8005ca5529b3f7d7333f1e
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 16:13:17 +0930

    to waste raylor raids time, formatting.

--- lua/tweakdata.lua
+++ lua/tweakdata.lua
@@ -5,87 +5,26 @@ tweak_data.projectiles.rocket_frag.damage = 15000 -- hrl-7
 
 tweak_data.casino = {
 	unlock_level = 0,
-	entrance_level = {
-		14,
-		28,
-		40,
-		45,
-		55,
-		65,
-		75
-	},
-	entrance_fee = {
-		15000,
-		15000,
-		15000,
-		15000,
-		15000,
-		15000,
-		15000
-	},
+	entrance_level = { 14, 28, 40, 45, 55, 65, 75 },
+	entrance_fee = { 15000, 15000, 15000, 15000, 15000, 15000, 15000 },
 	prefer_cost = 500000,
 	prefer_chance = 0.2,
-	secure_card_cost = {
-		1000000,
-		3300000,
-		6500000
-	},
-	secure_card_level = {
-		0,
-		0,
-		0
-	},
+	secure_card_cost = { 1000000, 3300000, 6500000 },
+	secure_card_level = { 0, 0, 0 },
 	infamous_cost = 3000000,
 	infamous_chance = 3
 }	
 
 tweak_data.experience_manager.stage_failed_multiplier = 0.5
 -- From update 34
-tweak_data.experience_manager.stage_completion = {
-	200,
-	250,
-	300,
-	350,
-	425,
-	475,
-	550
-}
-tweak_data.experience_manager.job_completion = {
-	2000,
-	4000,
-	10000,
-	16000,
-	20000,
-	28000,
-	32000
-}
+tweak_data.experience_manager.stage_completion = { 200, 250, 300, 350, 425, 475, 550 }
+tweak_data.experience_manager.job_completion = { 2000, 4000, 10000, 16000, 20000, 28000, 32000 }
 
-tweak_data.experience_manager.pro_day_multiplier = {
-	1,
-	1,
-	1,
-	1,
-	1,
-	1,
-	1
-}
+tweak_data.experience_manager.pro_day_multiplier = { 1, 1, 1, 1, 1, 1, 1 }
 
 -- exp card drops give more xp now
-tweak_data.experience_manager.loot_drop_value = {
-	xp10 = 8000,
-	xp15 = 16000,
-	xp20 = 32000,
-	xp30 = 64000,
-	xp40 = 96000,
-	xp50 = 128000,
-	xp60 = 192000,
-	xp70 = 256000,
-	xp80 = 320000,
-	xp90 = 384000,
-	xp100 = 512000,
-	xp_pda9_1 = 250000,
-	xp_pda9_2 = 3000000,
-}
+tweak_data.experience_manager.loot_drop_value = { xp10 = 8000, xp15 = 16000, xp20 = 32000, xp30 = 64000, xp40 = 96000, xp50 = 128000, xp60 = 192000,
+												  xp70 = 256000, xp80 = 320000, xp90 = 384000, xp100 = 512000, xp_pda9_1 = 250000, xp_pda9_2 = 3000000, }
 
 -- if on one down, add 50% extra xp.
 tweak_data.experience_manager.pro_job_multiplier = 1.50
