553c548da12fa8b1afe778b943a9ced8b1b0cf1a
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 21:24:40 +0930

    change save file as an attempt to fix a cloud save popup...

--- lua/savefilemanager.lua
+++ lua/savefilemanager.lua
@@ -1,3 +1,3 @@
 -- changes save files, as otherwise your vanilla save will get fucked by the skilltree changes
-SavefileManager.PROGRESS_SLOT = 612
-SavefileManager.BACKUP_SLOT = 612
+SavefileManager.PROGRESS_SLOT = 033
+SavefileManager.BACKUP_SLOT = 033
