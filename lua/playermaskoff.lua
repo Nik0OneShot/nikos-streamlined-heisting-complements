6f350bdf17c836688cfdf71610625f707ee4cc9f
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 17:25:53 +0930

    add downsides to sprinting and crouching in casing mode.

--- lua/playermaskoff.lua
+++ lua/playermaskoff.lua
@@ -1,6 +1,18 @@
 -- using this now https://modworkshop.net/mod/50586
 
 local level_id = Global.level_data and Global.level_data.level_id or ""
+local SPRINT_SUSPICION_MULTIPLIER = 2
+local CROUCH_SUSPICION_MULTIPLIER = 1.5
+local SUSPICION_REASON = "casing_movement"
+
+local function update_movement_suspicion(self)
+    local multiplier = 1
+    if self._running then multiplier = SPRINT_SUSPICION_MULTIPLIER
+    elseif self._state_data.ducking then multiplier = CROUCH_SUSPICION_MULTIPLIER end
+    local player_base = self._unit:base()
+    player_base:set_suspicion_multiplier(SUSPICION_REASON, multiplier)
+    player_base:set_detection_multiplier(SUSPICION_REASON, multiplier)
+end
 
 Hooks:PostHook(PlayerMaskOff, "_check_action_duck", "CheckDuckAndJump", function(self, t, input)
    
@@ -46,11 +58,8 @@ Hooks:PostHook(PlayerMaskOff, "_check_action_interact", "CheckInteract", functio
 end)
 
 Hooks:OverrideFunction(PlayerMaskOff, "_upd_attention", function(self)
-    if self:_get_detection_status() == "hostile" or self._is_suspicious then
-        PlayerStandard._upd_attention(self)
-    else
-        self._ext_movement:set_attention_settings(self._mask_off_attention_settings)
-    end
+    update_movement_suspicion(self)
+    self._ext_movement:set_attention_settings(self._mask_off_attention_settings)
 end)
 
 Hooks:OverrideFunction(PlayerMaskOff, "_play_equip_animation", function(self) end)
@@ -105,6 +114,12 @@ Hooks:OverrideFunction(PlayerMaskOff, "_end_action_running", function(self, t)
     if not self._end_running_expire_t then
         self._end_running_expire_t = t + 0.4
     end
+end)
+
+Hooks:PostHook(PlayerMaskOff, "exit", "CasingMovementExit", function(self)
+    local player_base = self._unit:base()
+    player_base:set_suspicion_multiplier(SUSPICION_REASON, 1)
+    player_base:set_detection_multiplier(SUSPICION_REASON, 1)
 end)
 
 Hooks:PostHook(PlayerMaskOff, "enter", "fix_detection", function(self)
