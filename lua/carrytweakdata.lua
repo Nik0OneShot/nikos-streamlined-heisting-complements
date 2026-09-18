cf7ca4537461341d68f6cc39b42af82569291f65
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-19 07:31:48 +0930

    FUCK

--- lua/carrytweakdata.lua
+++ lua/carrytweakdata.lua
@@ -4,6 +4,6 @@ Hooks:PostHook(CarryTweakData, "init", "run with everything lol", function(self)
 		v.can_run = true
 	    v.move_speed_modifier = math.min(v.move_speed_modifier + 0.15, 1)
 	    v.jump_modifier = math.min(v.jump_modifier + 0.25, 1)
-	    v.throw_distance_multiplier = math.min(v.throw_distance_multiplier + 0.25, 1
+	    v.throw_distance_multiplier = math.min(v.throw_distance_multiplier + 0.25, 1)
     end
 end)
