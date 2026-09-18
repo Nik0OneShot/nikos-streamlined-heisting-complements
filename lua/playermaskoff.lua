a539f0228865fe0fa26f9da9b1927e679ebef916
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-19 08:33:02 +0930

    hopefully this'll work

--- lua/playermaskoff.lua
+++ lua/playermaskoff.lua
@@ -8,17 +8,17 @@ local CROUCH_DETECTION_MULTIPLIER = 6.25
 local SUSPICION_REASON = "casing_movement"
 
 local function update_movement_suspicion(self)
+	local player_base = self._unit:base()
 	local suspicion_multiplier = 1
 	local detection_multiplier = 1
 	if self._running then
 		suspicion_multiplier = SPRINT_SUSPICION_MULTIPLIER
 		detection_multiplier = SPRINT_DETECTION_MULTIPLIER
-		player_base._detection_settings.delay_mul = player_base._detection_settings.delay_mul * detection_multiplier
+		player_base:_setup_suspicion_and_detection_data.init_delay_mul = 0 -- remove that shit if sprinting
 	elseif self._state_data.ducking then
 		suspicion_multiplier = CROUCH_SUSPICION_MULTIPLIER
 		detection_multiplier = CROUCH_DETECTION_MULTIPLIER
 	end
-	local player_base = self._unit:base()
 	player_base:set_suspicion_multiplier(SUSPICION_REASON, suspicion_multiplier)
 	player_base:set_detection_multiplier(SUSPICION_REASON, detection_multiplier)
 end
