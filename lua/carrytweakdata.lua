68ede0b5270869803058118d01f75574077a1f45
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-19 07:30:22 +0930

    tweak throw distance multipliers

--- lua/carrytweakdata.lua
+++ lua/carrytweakdata.lua
@@ -1,8 +1,9 @@
 Hooks:PostHook(CarryTweakData, "init", "run with everything lol", function(self)
--- use eclipse code to modify bag move speed and jump mods
+-- use eclipse code to modify bag move speed and jump mods, then apply my own changes.
     for _, v in pairs(self.types) do
 		v.can_run = true
 	    v.move_speed_modifier = math.min(v.move_speed_modifier + 0.15, 1)
 	    v.jump_modifier = math.min(v.jump_modifier + 0.25, 1)
+	    v.throw_distance_multiplier = math.min(v.throw_distance_multiplier + 0.25, 1
     end
 end)
