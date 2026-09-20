02949d5720546871d0455691db797d756fa5c30c
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-20 18:52:07 +0930

    remove this to properly demonstrate that

--- lua/weaponfactorytweakdata.lua
+++ lua/weaponfactorytweakdata.lua
@@ -229,7 +229,7 @@ Hooks:PostHook(WeaponFactoryTweakData, "init", "shc_init", function(self)
 	-- rami suppressor
 	self.parts.wpn_fps_lmg_kacchainsaw_ns_suppressor.stats.damage = 0
 	self.parts.wpn_fps_lmg_kacchainsaw_ns_suppressor.stats.spread = 1
-	self.parts.wpn_fps_lmg_kacchainsaw_ns_suppressor.stats.recoil = -1
+	self.parts.wpn_fps_lmg_kacchainsaw_ns_suppressor.stats.recoil = 0
 	self.parts.wpn_fps_lmg_kacchainsaw_ns_suppressor.stats.concealment = -1
 		
 	-- Set lowest alert range for all suppressors
