2f18586cb693cc802a97b9cc235162e989b3cdaf
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-20 18:46:21 +0930

    give the ks12-s ludicrous stats, but an extreme concealment penalty.

--- lua/weaponfactorytweakdata.lua
+++ lua/weaponfactorytweakdata.lua
@@ -219,6 +219,12 @@ Hooks:PostHook(WeaponFactoryTweakData, "init", "shc_init", function(self)
 	self.parts.wpn_fps_upg_ns_ass_smg_large.stats.spread = 3
 	self.parts.wpn_fps_upg_ns_ass_smg_large.stats.recoil = 2
 	self.parts.wpn_fps_upg_ns_ass_smg_large.stats.concealment = -4
+
+	-- ks12-s long suppressor
+	self.parts.wpn_fps_ass_shak12_ns_suppressor.damage = 0
+	self.parts.wpn_fps_ass_shak12_ns_suppressor.stats.spread = 5
+	self.parts.wpn_fps_ass_shak12_ns_suppressor.stats.recoil = 4
+	self.parts.wpn_fps_ass_shak12_ns_suppressor.stats.concealment = -8
 		
 	-- Set lowest alert range for all suppressors
 	-- Tweak Sting grenade pickup
